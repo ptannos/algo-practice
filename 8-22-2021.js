dc95db6ba089abf1460e5f7e65775f002757ea0c
Pauline Tannos <ptannos2@gmail.com>
2021-08-22 16:49:55 -0400

    minimum waiting time

--- 8-22-2021.js
+++ 8-22-2021.js
@@ -55,3 +55,39 @@ function sumEachBranch(node, sumArr = [], currSum = 0) {
 // Do not edit the lines below.
 exports.BinaryTree = BinaryTree;
 exports.branchSums = branchSums;
+
+// MINIMUM WAITING TIME
+
+// You're given a non-empty array of positive integers
+// representing the amounts of time that specific queries
+// take to execute. Only one query can be executed at a
+// time, but the queries can be executed in any order.
+
+// A query's waiting time is defined as the amount of time
+// that it must wait before its execution starts. In other
+// words, if a query is executed second, then its waiting
+// time is the duration of the first query; if a query is
+// executed third, then its waiting time is the sum of the
+// durations of the first two queries.
+
+// Write a function that returns the minimum amount of total
+// waiting time for all the queries. For example, if you're given
+// the queries of durations [1, 4, 5] then total waiting time if
+// the queries were executed in the order of [5, 1, 4] would be
+// (0) + (5) + (5+1) = 11. The first query of duration 5 would be
+// executed immediately, so its waiting time would be 0, the
+// second query of duration 1 would have to wait 5 seconds, and
+// the last would have to wait the duration of the first 2 queries.
+
+function minimumWaitingTime(queries) {
+  queries.sort((a, b) => a - b);
+
+  let sum = 0;
+
+  for (let i = 0; i < queries.length; i++) {
+    const query = queries[i];
+    const queriesLeft = queries.length - (i + 1);
+    sum += queriesLeft * query;
+  }
+  return sum;
+}
