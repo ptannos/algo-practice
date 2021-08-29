ae5dd7ad3d55a2afeb780dfa84df37f05fea6f7a
Pauline Tannos <ptannos2@gmail.com>
2021-08-28 22:33:20 -0400

    node depths updated

---
+++ 8-28-2021.js
@@ -0,0 +1,9 @@
+/* NODE DEPTHS
+
+The distance between a node in a Binary Tree and the tree's root
+is caled the node's depth. Write a function that takes in a binary
+tree and returns the sum of its nodes' depths. 
+
+Each BinaryTree node has an integer value, a left child node, and a
+right child node. Children nodes can either be BoinaryTree nodes
+themselves or None / null. */
