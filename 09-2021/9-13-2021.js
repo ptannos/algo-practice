392213e8909d0dfd5e566854be36263617f61056
Pauline Tannos <ptannos2@gmail.com>
2021-09-13 21:07:34 -0400

    three number sum

---
+++ 09-2021/9-13-2021.js
@@ -0,0 +1,11 @@
+/* THREE NUMBER SUM
+
+Write a function that takes in a non-empty array of distinct
+integers and an integer representing a target sum. The function
+should find all triplets in the array that sum up to the target
+sum and return a two-dimensional array of all these triples. The
+numbers in each triplet should be ordered in ascending order, and 
+the triplets should be ordered in ascending order. 
+
+If no three numbers sum up to the target sum, the function should
+return an empty array. */
