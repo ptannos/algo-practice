fa70d577319ed6426101ada79f035742f3c8095c
Pauline Tannos <ptannos2@gmail.com>
2021-09-14 17:37:33 -0400

    three num sum solution

--- 09-2021/9-13-2021.js
+++ 09-2021/9-13-2021.js
@@ -9,3 +9,27 @@ the triplets should be ordered in ascending order.
 
 If no three numbers sum up to the target sum, the function should
 return an empty array. */
+
+function threeNumberSum(array, targetSum) {
+  array.sort((a, b) => a - b);
+
+  let results = [];
+
+  for (let i = 0; i < array.length; i++) {
+    let pointer1 = i + 1;
+    let pointer2 = array.length - 1;
+
+    while (pointer1 < pointer2) {
+      if (array[i] + array[pointer1] + array[pointer2] === targetSum) {
+        results.push([array[i], array[pointer1], array[pointer2]]);
+        pointer1++;
+        pointer2--;
+      } else if (array[i] + array[pointer1] + array[pointer2] < targetSum) {
+        pointer1++;
+      } else {
+        pointer2--;
+      }
+    }
+  }
+  return results;
+}
